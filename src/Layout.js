48b972f97feb53b8158eed84cec233a687e9cc29
Komang Eric Widhi Antara <nukenin.konoha@gmail.com>
2018-03-22 21:59:07 +0800

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -12,19 +12,19 @@ export default class BelajarLayout extends React.Component {
       <View style={styles.box2}>
           <Text style={{ textAlign: 'center', padding: 30, fontSize: 20 }}> Slider </Text>
       </View>
-      <View style={styles.box4}>
+      <View style={styles.box3}>
         <View style={styles.button}><Text> 1 </Text></View>
         <View style={styles.button}><Text> 2 </Text></View>
         <View style={styles.button}><Text> 3 </Text></View>
         <View style={styles.button}><Text> 4 </Text></View> 
       </View>
-      <View style={styles.box4}>
+      <View style={styles.box3}>
         <View style={styles.button}><Text> 5 </Text></View>
         <View style={styles.button}><Text> 6 </Text></View>
         <View style={styles.button}><Text> 7 </Text></View>
         <View style={styles.button}><Text> 8 </Text></View> 
       </View>
-      <View style={styles.box5}>
+      <View style={styles.box4}>
           <Text style={{ textAlign: 'center', color: 'white', padding: 30, fontSize: 20 }}> #JaenKuliahdiPTI</Text>
       </View>
 
@@ -48,18 +48,8 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-around',
     alignItems: 'center'
-  },
+  }, 
   box3: {
-    flex: 0.5,
-    backgroundColor: '#90CAF9',
-    //marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'row'
-  },
-  box4: {
     flex: 0.7,
     backgroundColor: '#90CAF9',
     //marginTop: 10,
@@ -69,7 +59,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row'
   },
-  box5: {
+  box4: {
     flex: 0.7,
     backgroundColor: '#1565C0',
     margin: 10
